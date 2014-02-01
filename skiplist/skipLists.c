5982c9ac13319acc120712a5a8bc03381e758fc4
zheng-ji <407562752@qq.com>
2014-02-01 10:59:46 +0800

    跳表

--- skiplist/skipLists.c
+++ skiplist/skipLists.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 #define MAX_LEVEL 10
-typedef struct nodeStructure {
+typedef struct node {
     int value;
     int key;
-    struct nodeStructure *forward[1];
-} nodeStructure;
+    struct node *next[1];
+} node;
 
 typedef struct skiplist {
     int level;
-    nodeStructure *header;
+    node *header;
 } skiplist;
 
-nodeStructure *createNode(int level, int key, int value) {
-    nodeStructure *ns = (nodeStructure *) malloc(sizeof(nodeStructure) + level * sizeof(nodeStructure *));
+node *createNode(int level, int key, int value) {
+    node *ns = (node *) malloc(sizeof(node) + level * sizeof(node *));
     ns->key = key;
     ns->value = value;
     return ns;
@@ -26,7 +26,7 @@ skiplist * createSkiplist() {
     sl->header = createNode(MAX_LEVEL-1, 0, 0);
     int i;
     for (i = 0; i < MAX_LEVEL; i++) {
-        sl->header->forward[i] = NULL;
+        sl->header->next[i] = NULL;
     }
     return sl;
 }
@@ -40,13 +40,13 @@ int randomLevel() {
 }
 
 bool insert(skiplist *sl, int key, int value) {
-    nodeStructure *update[MAX_LEVEL];
-    nodeStructure *p, *q = NULL;
+    node *update[MAX_LEVEL];
+    node *p, *q = NULL;
     p = sl->header;
     int k = sl->level;
     int i;
     for (i = k - 1; i >= 0; i--) {
-        while ((q = p->forward[i]) && (q->key < key)) {
+        while ((q = p->next[i]) && (q->key < key)) {
             p = q;
         }
         update[i] = p;
@@ -63,18 +63,18 @@ bool insert(skiplist *sl, int key, int value) {
     }
     q = createNode(k, key, value);
     for (int i = 0 ; i < k; i++) {
-        q->forward[i] = update[i]->forward[i];
-        update[i] -> forward[i] = q;
+        q->next[i] = update[i]->next[i];
+        update[i] -> next[i] = q;
     }
     return true;
 }
 
 int search(skiplist * sl, int key) {
-    nodeStructure * p, *q = NULL;
+    node * p, *q = NULL;
     p = sl->header;
     int k = sl->level;
     for (int i = k-1; i >= 0; i--) {
-        while ((q=p->forward[i]) && q->key <= key) {
+        while ((q=p->next[i]) && q->key <= key) {
             if (q->key == key) {
                 return q->value;
             }
@@ -85,12 +85,12 @@ int search(skiplist * sl, int key) {
 }
 
 bool deleteSL(skiplist * sl, int key) {
-    nodeStructure *update[MAX_LEVEL];
-    nodeStructure *p, *q = NULL;
+    node *update[MAX_LEVEL];
+    node *p, *q = NULL;
     p = sl ->header;
     int k = sl->level;
     for (int i = k-1; i >= 0; i--) {
-        while ((q=p->forward[i]) && (q->key < key)) {
+        while ((q=p->next[i]) && (q->key < key)) {
             p=q;
         }
         update[i] = p;
@@ -99,13 +99,13 @@ bool deleteSL(skiplist * sl, int key) {
     //删除最大的，要更新节点
     if (q&&q->key==key) {
         for (int i = 0; i < sl->level; i++) {
-            if (update[i]->forward[i]==q) {
-                update[i]->forward[i] == q->forward[i];
+            if (update[i]->next[i]==q) {
+                update[i]->next[i] == q->next[i];
             }
         }
         free(q);
         for (int i = sl->level;i >= 0; i--) {
-            if (sl->header->forward[i] == NULL) {
+            if (sl->header->next[i] == NULL) {
                 sl->level --;
             }
         }
@@ -116,11 +116,11 @@ bool deleteSL(skiplist * sl, int key) {
 }
 
 void printSL(skiplist *sl) {
-    nodeStructure *p, *q = NULL;
+    node *p, *q = NULL;
     int k = sl->level;
     for (int i = k-1;i >= 0; i--) {
         p = sl->header;
-        while (q = p->forward[i]) {
+        while (q = p->next[i]) {
             printf("%d->", p->value);
             p = q;
         }
