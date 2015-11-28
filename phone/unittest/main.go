62e7b809f48b3c1c74d8e46171b42b28183c60d9
zheng-ji <407562752@qq.com>
2015-11-28 10:44:05 +0800

    修改代码

--- phone/unittest/main.go
+++ phone/unittest/main.go
@@ -3,18 +3,40 @@ package main
 import (
     "fmt"
 	"phone/unittest/phone"
+    "time"
 )
 
+func Since(t time.Time) int {
+    return int(time.Since(t).Nanoseconds() / 1e6)
+}
+
 func main() {
+    timeStart := time.Now()
 	phone.Load("phone.dat")
-    phone.Display()
-	/*fmt.Println(phone.Find("15920554608"))*/
-    /*pr := phone.Find("13580198235")*/
+    fmt.Println(Since(timeStart))
     fmt.Println("=====")
-    /*fmt.Println(pr.PhoneNum)*/
-    /*fmt.Println(pr.AreaZone)*/
-    /*fmt.Println(pr.CardType)*/
-    /*fmt.Println(pr.City)*/
-    /*fmt.Println(pr.ZipCode)*/
-    /*fmt.Println(pr.Province)*/
+    timeStart = time.Now()
+    phone.Display()
+    fmt.Println(Since(timeStart))
+    fmt.Println("=====")
+    timeStart = time.Now()
+    pr := phone.Find("13580198235")
+    fmt.Println(pr.PhoneNum)
+    fmt.Println(pr.AreaZone)
+    fmt.Println(pr.CardType)
+    fmt.Println(pr.City)
+    fmt.Println(pr.ZipCode)
+    fmt.Println(pr.Province)
+    fmt.Println(Since(timeStart))
+    fmt.Println("=====")
+    timeStart = time.Now()
+    pr = phone.Find("1581381745")
+    fmt.Println(pr.PhoneNum)
+    fmt.Println(pr.AreaZone)
+    fmt.Println(pr.CardType)
+    fmt.Println(pr.City)
+    fmt.Println(pr.ZipCode)
+    fmt.Println(pr.Province)
+    fmt.Println(Since(timeStart))
+    fmt.Println("=====")
 }
