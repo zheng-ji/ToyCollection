4044a3c76e66da47c8e9f9908ff885e3763efe4f
zheng-ji <407562752@qq.com>
2016-09-25 22:51:26 +0800

    add 共享内存

--- shared_memory/shmdata.h
+++ shared_memory/shmdata.h
@@ -15,52 +15,52 @@ struct shared_msg
 
 static int set_semvalue(int sem_id)
 {
-	//用于初始化信号量，在使用信号量前必须这样做
-	union semun sem_union;
+    //用于初始化信号量，在使用信号量前必须这样做
+    union semun sem_union;
 
-	sem_union.val = 1;
-	if(semctl(sem_id, 0, SETVAL, sem_union) == -1)
-		return 0;
-	return 1;
+    sem_union.val = 1;
+    if(semctl(sem_id, 0, SETVAL, sem_union) == -1)
+        return 0;
+    return 1;
 }
 
 static void del_semvalue(int sem_id)
 {
-	//删除信号量
-	union semun sem_union;
+    //删除信号量
+    union semun sem_union;
 
-	if(semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
-		fprintf(stderr, "Failed to delete semaphore\n");
+    if(semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+        fprintf(stderr, "Failed to delete semaphore\n");
 }
 
 static int semaphore_p(int sem_id)
 {
-	//对信号量做减1操作，即等待P（sv）
-	struct sembuf sem_b;
-	sem_b.sem_num = 0;
-	sem_b.sem_op = -1;//P()
-	sem_b.sem_flg = SEM_UNDO;
-	if(semop(sem_id, &sem_b, 1) == -1)
-	{
-		fprintf(stderr, "semaphore_p failed\n");
-		return 0;
-	}
-	return 1;
+    //对信号量做减1操作，即等待P（sv）
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = -1;//P()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        fprintf(stderr, "semaphore_p failed\n");
+        return 0;
+    }
+    return 1;
 }
 
 static int semaphore_v(int sem_id)
 {
-	//这是一个释放操作，它使信号量变为可用，即发送信号V（sv）
-	struct sembuf sem_b;
-	sem_b.sem_num = 0;
-	sem_b.sem_op = 1;//V()
-	sem_b.sem_flg = SEM_UNDO;
-	if(semop(sem_id, &sem_b, 1) == -1)
-	{
-		fprintf(stderr, "semaphore_v failed\n");
-		return 0;
-	}
-	return 1;
+    //这是一个释放操作，它使信号量变为可用，即发送信号V（sv）
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;
+    sem_b.sem_op = 1;//V()
+    sem_b.sem_flg = SEM_UNDO;
+    if(semop(sem_id, &sem_b, 1) == -1)
+    {
+        fprintf(stderr, "semaphore_v failed\n");
+        return 0;
+    }
+    return 1;
 }
 
 #endif
--- shared_memory/shmwrite.c
+++ shared_memory/shmwrite.c
@@ -10,18 +10,18 @@
 
 int main() {
 
-	int running = 1;
-	void *shm = NULL;
-	struct shared_msg *shared = NULL;
+    int running = 1;
+    void *shm = NULL;
+    struct shared_msg *shared = NULL;
 
     //用于保存输入的文本
-	char buffer[BUFSIZ + 1];
-    
+    char buffer[BUFSIZ + 1];
+
     // 执行信号量
     int sem_id = 0;
 
-	//创建信号量
-	sem_id = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
+    //创建信号量
+    sem_id = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
 
     if (!set_semvalue(sem_id)) {
         fprintf(stderr, "Failed to initialize semaphore\n");
@@ -29,53 +29,53 @@ int main() {
     }
 
     //共享内存标识符 int shmid;
-	int shmid;
+    int shmid;
 
-	//创建共享内存
-	shmid = shmget(MY_KEY, sizeof(struct shared_msg), 0666|IPC_CREAT);
-	if (shmid == -1) {
-		fprintf(stderr, "shmget failed\n");
-		exit(EXIT_FAILURE);
-	}
+    //创建共享内存
+    shmid = shmget(MY_KEY, sizeof(struct shared_msg), 0666|IPC_CREAT);
+    if (shmid == -1) {
+        fprintf(stderr, "shmget failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	//将共享内存连接到当前进程的地址空间
-	shm = shmat(shmid, (void*)0, 0);
-	if (shm == (void*)-1) {
-		fprintf(stderr, "shmat failed\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("Memory attached at %X\n", (int)shm);
+    //将共享内存连接到当前进程的地址空间
+    shm = shmat(shmid, (void*)0, 0);
+    if (shm == (void*)-1) {
+        fprintf(stderr, "shmat failed\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Memory attached at %X\n", (int)shm);
 
-	//设置共享内存
-	shared = (struct shared_msg*)shm;
-    
+    //设置共享内存
+    shared = (struct shared_msg*)shm;
+
     //向共享内存中写数据
-	while (running) {
+    while (running) {
 
-		if(!semaphore_p(sem_id))
-			exit(EXIT_FAILURE);
+        if(!semaphore_p(sem_id))
+            exit(EXIT_FAILURE);
 
-		//向共享内存中写入数据
-		printf("Enter text: ");
-		fgets(buffer, BUFSIZ, stdin);
-		strncpy(shared->msg, buffer, TEXT_SZ);
+        //向共享内存中写入数据
+        printf("Enter text: ");
+        fgets(buffer, BUFSIZ, stdin);
+        strncpy(shared->msg, buffer, TEXT_SZ);
 
-		//写完数据，设置written使共享内存段可读
-		shared->isWritten = 1;
+        //写完数据，设置written使共享内存段可读
+        shared->isWritten = 1;
 
-		//输入了end，退出循环（程序）
-		if(strncmp(buffer, "end", 3) == 0)
-			running = 0;
+        //输入了end，退出循环（程序）
+        if(strncmp(buffer, "end", 3) == 0)
+            running = 0;
 
-		if(!semaphore_v(sem_id))
-			exit(EXIT_FAILURE);
-	}
+        if(!semaphore_v(sem_id))
+            exit(EXIT_FAILURE);
+    }
 
-	//把共享内存从当前进程中分离
-	if (shmdt(shm) == -1) {
-		fprintf(stderr, "shmdt failed\n");
-		exit(EXIT_FAILURE);
-	}
-	sleep(2);
-	exit(EXIT_SUCCESS);
+    //把共享内存从当前进程中分离
+    if (shmdt(shm) == -1) {
+        fprintf(stderr, "shmdt failed\n");
+        exit(EXIT_FAILURE);
+    }
+    sleep(2);
+    exit(EXIT_SUCCESS);
 }
